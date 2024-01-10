4c26b5f51fb32b03d6155adb0641598d9bf99e2d
Mirac Gulgonul <mlg556@gmail.com>
2024-01-11 00:24:53 +0300

    added error prints

--- src/ast.zig
+++ src/ast.zig
@@ -34,12 +34,18 @@ pub const Program = struct {
 };
 
 pub const Parser = struct {
+    ///  A pointer to an instance of the lexer, on which we repeatedly call nextToken() to get the next token in the input.
     lex: *Lexer,
-    curToken: Token,
-    peekToken: Token,
+    /// points to the current token
+    curToken: Token = Token{ .type = .ILLEGAL, .literal = "" },
+    /// points to the next token
+    peekToken: Token = Token{ .type = .ILLEGAL, .literal = "" },
+    /// number of errors
+    errCount: u32 = 0,
 
+    /// initializes parser with a given lexer and allocator.
     pub fn init(l: *Lexer) Parser {
-        var p: Parser = .{ .lex = l, .curToken = undefined, .peekToken = undefined };
+        var p: Parser = .{ .lex = l };
 
         // Read two tokens, so curToken and peekToken are both set
         p.nextToken();
@@ -52,7 +58,7 @@ pub const Parser = struct {
         p.curToken = p.peekToken;
         p.peekToken = p.lex.nextToken();
     }
-
+    /// constructs the root node of the AST, an *ast.Program. iterates over every token in the input until it encounters an token.EOF.. In every iteration it calls parseStatement, whose job it is to parse a statement. If parseStatement returned something other than nil, a ast.Statement, its return value is added to Statements list of the AST root node. When nothing is left to parse the *ast.Program root node is returned.
     pub fn parseProgram(p: *Parser, alloc: std.mem.Allocator) !*Program {
         // program is an arraylist of statements, we keep lexing and pushing
         var program = Program.init(alloc);
@@ -105,19 +111,28 @@ pub const Parser = struct {
         return p.peekToken.type == t;
     }
 
+    /// The expectPeek method is one of the “assertion functions” nearly all parsers share. Their primary purpose is to enforce the correctness of the order of tokens by checking the type of the next token. Our expectPeek here checks the type of the peekToken and only if the type is correct does it advance the tokens by calling nextToken.
     pub fn expectPeek(p: *Parser, t: TokenType) bool {
         if (p.peekTokenIs(t)) {
             p.nextToken();
             return true;
         }
+        p.peekError(t);
         return false;
+    }
+
+    // Instead of allocating an arraylist for errors, we just hold an error counter and debug.print them for now.
+    pub fn peekError(p: *Parser, t: TokenType) void {
+        // increment error count
+        p.errCount += 1;
+        std.debug.print("ERR: expected token {s}, got {s}.", .{ @tagName(t), @tagName(p.peekToken.type) });
     }
 };
 
 test "AST" {
     const input =
-        \\let x = 5;
-        \\let ya = 53;
+        \\let x = 3 ;
+        // \\let ya = 53;
     ;
 
     // zig fmt: off
@@ -145,8 +160,13 @@ test "AST" {
     var prog = try parser.parseProgram(allocator);
 
     std.debug.print("\n", .{});
+
+    // no peek errors
+    try std.testing.expect(parser.errCount == 0);
+
     for (prog.statements.items, 0..) |stmt, i| {
-        std.debug.print("{:}\n", .{stmt});
+        // std.debug.print("{:}\n", .{stmt});
+
         try std.testing.expectEqualDeep(exp_stmts[i], stmt);
         //try std.json.stringify(stmt, .{}, std.io.getStdOut().writer());
     }
