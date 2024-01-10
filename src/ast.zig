66e39928d36fec4e24ae61f3eed34c650bde4502
Mirac Gulgonul <mlg556@gmail.com>
2024-01-10 21:18:30 +0300

    parselet working?

--- src/ast.zig
+++ src/ast.zig
@@ -1,12 +1,11 @@
 const std = @import("std");
 
-const lex = @import("lexer.zig");
-const Token = lex.Token;
-const Lexer = lex.Lexer;
-const string = lex.string;
+const L = @import("lexer.zig");
+const Token = L.Token;
+const TokenType = L.TokenType;
+const Lexer = L.Lexer;
 
-// a program can consist of this many statements? no heap?
-pub const N = 100;
+const string = []const u8;
 
 pub const Expression = struct {};
 
@@ -14,7 +13,15 @@ pub const Identifier = struct { token: Token, value: string };
 
 pub const Statement = struct { token: Token, name: Identifier, value: Expression };
 
-pub const Program = struct { statements: std.ArrayList(Statement) };
+pub const Program = struct {
+    statements: std.ArrayList(Statement),
+
+    pub fn init(alloc: std.mem.Allocator) Program {
+        var statements = std.ArrayList(Statement).init(alloc);
+
+        return .{ .statements = statements };
+    }
+};
 
 pub const Parser = struct {
     lex: *Lexer,
@@ -22,9 +29,7 @@ pub const Parser = struct {
     peekToken: Token,
 
     pub fn init(l: *Lexer) Parser {
-        var p = .{
-            .lex = l,
-        };
+        var p: Parser = .{ .lex = l, .curToken = undefined, .peekToken = undefined };
 
         // Read two tokens, so curToken and peekToken are both set
         p.nextToken();
@@ -38,36 +43,78 @@ pub const Parser = struct {
         p.peekToken = p.lex.nextToken();
     }
 
-    pub fn parseProgram(p: *Parser, alloc: std.mem.Allocator) *Program {
+    pub fn parseProgram(p: *Parser, alloc: std.mem.Allocator) !*Program {
         // program is an arraylist of statements, we keep lexing and pushing
-        var program: Program = .{ .statements = std.ArrayList(Statement).init(alloc) };
+        var program = Program.init(alloc);
 
-        while (!p.curToken.isEOF()) {
+        while (!p.curTokenIs(TokenType.EOF)) {
             var stmt = p.parseStatement();
 
-            if (stmt) |stat| {
-                program.statements.append(stat);
+            if (stmt) |statement| {
+                try program.statements.append(statement);
             }
 
             p.nextToken();
         }
 
-        return program;
+        return &program;
     }
 
     pub fn parseStatement(p: *Parser) ?Statement {
-        switch (p.curToken) {
+        switch (p.curToken.type) {
             .LET => return p.parseLetStatement(),
             else => return null,
         }
     }
 
-    pub fn parseLetStatement(p: *Parser) ?*Statement {
+    pub fn parseLetStatement(p: *Parser) ?Statement {
         var stmt = Statement{ .token = p.curToken, .name = undefined, .value = undefined };
 
-        if (!p.expectPeek())
-            _ = stmt;
+        if (!p.expectPeek(TokenType.IDENT))
+            return null;
+
+        stmt.name = .{ .token = p.curToken, .value = p.curToken.literal };
+
+        if (!p.expectPeek(TokenType.ASSIGN))
+            return null;
+
+        // TODO: We're skipping the expressions until we
+        // encounter a semicolon
+        while (!p.curTokenIs(TokenType.SEMICOLON)) {
+            p.nextToken();
+        }
+
+        return stmt;
     }
 
-    //pub fn expectPeek()
+    pub fn curTokenIs(p: *Parser, t: TokenType) bool {
+        return p.curToken.type == t;
+    }
+
+    pub fn peekTokenIs(p: *Parser, t: TokenType) bool {
+        return p.peekToken.type == t;
+    }
+
+    pub fn expectPeek(p: *Parser, t: TokenType) bool {
+        if (p.peekTokenIs(t)) {
+            p.nextToken();
+            return true;
+        }
+        return false;
+    }
 };
+
+test "AST" {
+    var buffer: [1024]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+
+    const input = "let x = 5;";
+    var lex = Lexer.init(input);
+    var parser = Parser.init(&lex);
+    var prog = try parser.parseProgram(allocator);
+
+    for (prog.statements.items) |stmt| {
+        std.debug.print("{:}\n", .{stmt});
+    }
+}
